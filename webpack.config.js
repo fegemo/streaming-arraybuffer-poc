a33400b128a14969b6ddfad777889c6ed1dfb7e7
Flávio Coutinho <fegemo@gmail.com>
2018-05-07 23:43:33 -0300

    Enables source maps to help debugging

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
